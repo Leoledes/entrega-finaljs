2bb64250567d4de7f650607101ae547024983020
Leoledes <ledesma.leonardoa@gmail.com>
2024-10-04 19:43:46 -0300

    estilos css 1

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-
 let cartStorage = localStorage.getItem("cartVinos")
 cartStorage = JSON.parse(cartStorage)
 
@@ -9,7 +7,7 @@ let cartContainer = document.getElementById("cart-section")
 //funcion para renderizar Carrito
 function renderCarrito (vinosArray) {
     if (!vinosArray || !vinosArray.length) {
-        cartContainer.innerHTML = 'Todavia no pusiste nada gato'
+        cartContainer.innerHTML = 'Te falta tomar más vino! Elegí uno que te guste y volvé al carrito.'
         return;
     }
     vinosArray.forEach(vino =>{
@@ -24,14 +22,15 @@ function renderCarrito (vinosArray) {
                             <button class="resta" id="restar-vino" data-js=${vino.id} > - </button>
                             <h4 id="cantidad-vino-${vino.id}">Cantidad:${vino.cantidad}</h4>
                             <button class="suma" id="sumar-vino" data-js=${vino.id}> + </button>
-                            <h4 id="precioSubtotal">Subtotal: $${vino.cantidad*vino.precio}</h4>
+                            <h4 id="precio-subtotal">Subtotal: $${vino.cantidad*vino.precio}</h4>
                           </div>
-                          <button class="eliminarVino" id="${vino.id}">Eliminar</button>`;                               
+                          <button class="elimina" id="eliminar-vino" data-js=${vino.id} > Eliminar </button>`                            
         cartContainer.appendChild(card)
     })
 
     const botonRestar = document.querySelectorAll('#restar-vino');
     const botonSumar = document.querySelectorAll('#sumar-vino');
+    const botonEliminar = document.querySelectorAll('#eliminar-vino');
 
     botonRestar.forEach(element => {
         element.addEventListener('click', (e) => {
@@ -60,6 +59,18 @@ function renderCarrito (vinosArray) {
             localStorage.setItem("cartVinos", JSON.stringify(vinosArray));
         })
     })
+    //TEMRINAR CARGA ERRONEA
+    botonEliminar.forEach(element => {
+        element.addEventListener('click', (e) => {
+            const vinoId = e.target.getAttribute('data-js');
+            const vinoStorage = vinosArray.find(vino => vino.id === vinoId);    
+            vinosArray.splice(vinoStorage, 1);
+            });
+
+            localStorage.setItem("cartVinos", JSON.stringify(vinosArray));
+
+        })
+    
 
     //boton Limpiar Carrito
     const botonLimpiar = document.createElement("div");
@@ -74,33 +85,7 @@ function renderCarrito (vinosArray) {
         renderCarrito();
     };
 }
-
-
 renderCarrito(cartStorage);
-})
-
-
-
-        
-    /* //funcion para contador de cantidad
-    let sumar = card.getElementsByClassName(".suma")
-    let restar = card.getElementsByClassName(".resta")
-    let cantidad = 0
-
-    sumar.onclick = () => {
-        vino.cantidad ++
-        restar.disabled = false
-    }
-
-    restar.onclick = () => {
-        if (vino.cantidad === 0){
-            restar.disabled = true
-        } else {
-            vino.cantidad --
-        }
-    } */
-
-
 
 
 
